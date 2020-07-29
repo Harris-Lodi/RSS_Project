283445604da8af1d3ee0dca6a14f909084973f34
Harris-Lodi <harrislodi90s@gmail.com>
2020-07-28 22:05:23 -0400

    Added a way to get an entry from inserted index value and key name
    Need to expand functionaility tomorrow!

--- RSS_Tag_Reader.py
+++ RSS_Tag_Reader.py
@@ -10,4 +10,19 @@ NewsFeed = feedparser.parse(get_input)
 # len(NewsFeed[entries])
 # NewsFeed.entries[0] for latest entry, NewsFeed.entries[-1] for last entry
 # NewsFeed.keys() for full html tags in RSS feed
-print(NewsFeed.entries[-1])
+print("HTML Tags for in URL Feed: ")
+useful_key = []
+for keys in NewsFeed.keys():
+    useful_key.append(keys)
+
+print(useful_key)
+
+print("\n")
+print("__________________________________________________________________")
+keyforuse = input("Please enter the key you want to access within RSS feed: ")
+entryindex = int(input("also please enter which entry you want to see, 0 for latest, -1 for oldest: "))
+
+keyindex = useful_key.index(keyforuse)
+values = NewsFeed.values()
+new_param = list(values)[keyindex][entryindex]
+print(new_param)
