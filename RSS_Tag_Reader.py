bee73b6933c9ad4572beb61f3a5464416280d915
Harris-Lodi <harrislodi90s@gmail.com>
2020-07-29 23:40:42 -0400

    Made it possible to get tags of a specific entry
    converted the entire dictionary to a list while maintaining the html tags and sub values for each item in list

--- RSS_Tag_Reader.py
+++ RSS_Tag_Reader.py
@@ -7,9 +7,8 @@ get_input = input("Please enter the URL of the RSS file to read: ")
 
 NewsFeed = feedparser.parse(get_input)
 
-# len(NewsFeed[entries])
-# NewsFeed.entries[0] for latest entry, NewsFeed.entries[-1] for last entry
-# NewsFeed.keys() for full html tags in RSS feed
+FeedList = list(NewsFeed.values())
+
 print("HTML Tags for in URL Feed: ")
 useful_key = []
 for keys in NewsFeed.keys():
@@ -20,9 +19,11 @@ print(useful_key)
 print("\n")
 print("__________________________________________________________________")
 keyforuse = input("Please enter the key you want to access within RSS feed: ")
+
 entryindex = int(input("also please enter which entry you want to see, 0 for latest, -1 for oldest: "))
 
 keyindex = useful_key.index(keyforuse)
-values = NewsFeed.values()
-new_param = list(values)[keyindex][entryindex]
-print(new_param)
+output_entry = FeedList[keyindex][entryindex]
+print(output_entry)
+
+print(output_entry.keys())
