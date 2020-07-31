b55a7346a4647031b3f1bd2b0af5b7a636b4698d
Harris-Lodi <harrislodi90s@gmail.com>
2020-07-30 23:36:24 -0400

    Added functionality to dig deeper
    script can now present keys for user to enter, and from those entry keys, the user can go deeper and get keys from a specific entry and get the value of that entry key! Uses try and except method to search for feed vs entries, the other tags are mostly untested but href doesn't work yet, need to seperate all these into different functions and need to add comments!

--- RSS_Tag_Reader.py
+++ RSS_Tag_Reader.py
@@ -19,11 +19,40 @@ print(useful_key)
 print("\n")
 print("__________________________________________________________________")
 keyforuse = input("Please enter the key you want to access within RSS feed: ")
-
-entryindex = int(input("also please enter which entry you want to see, 0 for latest, -1 for oldest: "))
-
+print("\n")
 keyindex = useful_key.index(keyforuse)
-output_entry = FeedList[keyindex][entryindex]
-print(output_entry)
 
-print(output_entry.keys())
+entryindex = 0
+
+try:
+    output_entry = FeedList[keyindex]
+    output_entry_keys = output_entry.keys()
+    output_entry_values = output_entry.values()
+    entry_keys = list(output_entry_keys)
+    entry_values = list(output_entry_values)
+except:
+    entryindex = int(input("also please enter which entry you want to see, 0 for latest, -1 for oldest: "))
+    print("\n")
+    output_entry = FeedList[keyindex][entryindex]
+    output_entry_keys = output_entry.keys()
+    output_entry_values = output_entry.values()
+    entry_keys = list(output_entry_keys)
+    entry_values = list(output_entry_values)
+
+print(output_entry)
+print("\n")
+print("Keys found in entry: ")
+print("\n")
+print(entry_keys)
+print("\n")
+entry_use_key = input("Please enter the entry key you want to see: ")
+
+useful_subkeys = []
+for keys in output_entry_keys:
+    useful_subkeys.append(keys)
+
+subkey = useful_subkeys.index(entry_use_key)
+
+entry_detail = entry_values[subkey]
+
+print(entry_detail)
