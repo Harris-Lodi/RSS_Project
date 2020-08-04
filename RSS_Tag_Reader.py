4df4702e11a86b74348a0736dd7980dbf540425b
Harris-Lodi <harrislodi90s@gmail.com>
2020-08-03 23:16:33 -0400

    Simplified the functions so that all uses can be done with only 2 functions
    Made things simpler while maintaining all current functionality

--- RSS_Tag_Reader.py
+++ RSS_Tag_Reader.py
@@ -6,12 +6,14 @@ os.system('cls') # clears the terminal from code, use 'clear' on linux/Mac
 get_input = input("Please enter the URL of the RSS file to read: ")
 
 NewsFeed = feedparser.parse(get_input)
+# "href", "status", "namespaces", "updated", "version", "headers", "bozo", "etag", "encoding"
+simpletaglist = []
 
-def entries(FeedList, keyindex):
+def non_simple(FeedList, keyindex):
 
     entryindex = 0
-    entryindex = int(input("also please enter which entry you want to see, 0 for latest, -1 for oldest: "))
     print("\n")
+    entryindex = int(input("also please enter which entry you want to see, 0 for latest, -1 for oldest: "))
     output_entry = FeedList[keyindex][entryindex]
     output_entry_keys = output_entry.keys()
     output_entry_values = output_entry.values()
@@ -36,87 +38,40 @@ def entries(FeedList, keyindex):
 
     print(entry_detail)
 
-def feed(FeedList, keyindex):
-
-    output_entry = FeedList[keyindex]
-    output_entry_keys = output_entry.keys()
-    output_entry_values = output_entry.values()
-    entry_keys = list(output_entry_keys)
-    entry_values = list(output_entry_values)
-
-    print(output_entry)
-    print("\n")
-    print("Keys found in entry: ")
-    print("\n")
-    print(entry_keys)
-    print("\n")
-    entry_use_key = input("Please enter the entry key you want to see: ")
-
-    useful_subkeys = []
-    for keys in output_entry_keys:
-        useful_subkeys.append(keys)
-
-    subkey = useful_subkeys.index(entry_use_key)
-
-    entry_detail = entry_values[subkey]
-
-    print(entry_detail)
-
-def hyperreference(FeedList, keyindex):
+def simple(FeedList, keyindex):
 
     output_entry = FeedList[keyindex]
 
-    print(output_entry)
+    try:
+        output_entry_keys = output_entry.keys()
+        output_entry_values = output_entry.values()
+        entry_keys = list(output_entry_keys)
+        entry_values = list(output_entry_values)
 
-def status(FeedList, keyindex):
+        print(output_entry)
+        print("\n")
+        print("Keys found in entry: ")
+        print("\n")
+        print(entry_keys)
+        print("\n")
+        entry_use_key = input("Please enter the entry key you want to see: ")
 
-    output_entry = FeedList[keyindex]
+        useful_subkeys = []
+        for keys in output_entry_keys:
+            useful_subkeys.append(keys)
 
-    print(output_entry)
+        subkey = useful_subkeys.index(entry_use_key)
 
-def namespaces(FeedList, keyindex):
+        entry_detail = entry_values[subkey]
 
-    output_entry = FeedList[keyindex]
+        print(entry_detail)
 
-    print(output_entry)
-
-def bozo(FeedList, keyindex):
-
-    output_entry = FeedList[keyindex]
-
-    print(output_entry)
-
-def feed_etag(FeedList, keyindex):
-
-    output_entry = FeedList[keyindex]
-
-    print(output_entry)
-
-def headers(FeedList, keyindex):
-
-    output_entry = FeedList[keyindex]
-
-    print(output_entry)
-
-def last_updated(FeedList, keyindex):
-
-    output_entry = FeedList[keyindex]
-
-    print(output_entry)
-
-def feed_encoding(FeedList, keyindex):
-
-    output_entry = FeedList[keyindex]
-
-    print(output_entry)
-
-def feed_version(FeedList, keyindex):
-
-    output_entry = FeedList[keyindex]
-
-    print(output_entry)
+    except:
+        print(output_entry)
 
 def find_url_tags():
+
+    global simpletaglist
 
     FeedList = list(NewsFeed.values())
 
@@ -125,6 +80,8 @@ def find_url_tags():
     for keys in NewsFeed.keys():
         useful_key.append(keys)
 
+    simpletaglist = useful_key.copy()
+    simpletaglist.remove("entries")
     print(useful_key)
 
     print("\n")
@@ -133,29 +90,13 @@ def find_url_tags():
     print("\n")
     keyindex = useful_key.index(keyforuse)
 
-    if keyforuse == useful_key[keyindex] == "feed":
-        feed(FeedList, keyindex)
-    elif keyforuse == useful_key[keyindex] == "entries":
-        entries(FeedList, keyindex)
-    elif keyforuse == useful_key[keyindex] == "href":
-        hyperreference(FeedList, keyindex)
-    elif keyforuse == useful_key[keyindex] == "status":
-        status(FeedList, keyindex)
-    elif keyforuse == useful_key[keyindex] == "namespaces":
-        namespaces(FeedList, keyindex)
-    elif keyforuse == useful_key[keyindex] == "updated":
-        last_updated(FeedList, keyindex)
-    elif keyforuse == useful_key[keyindex] == "version":
-        feed_version(FeedList, keyindex)
-    elif keyforuse == useful_key[keyindex] == "headers":
-        headers(FeedList, keyindex)
-    elif keyforuse == useful_key[keyindex] == "bozo":
-        bozo(FeedList, keyindex)
-    elif keyforuse == useful_key[keyindex] == "etag":
-        feed_etag(FeedList, keyindex)
-    elif keyforuse == useful_key[keyindex] == "encoding":
-        feed_encoding(FeedList, keyindex)
+    if keyforuse == useful_key[keyindex] == "entries":
+        non_simple(FeedList, keyindex)
+    elif any(x == keyforuse for x in simpletaglist):
+        simple(FeedList, keyindex)
     else:
         return "RSS Feed Read is done!"
+
+    del simpletaglist[:]
 
 find_url_tags()
